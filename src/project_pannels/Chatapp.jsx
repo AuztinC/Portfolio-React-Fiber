9ee55cf441a71c3fa059eeb8a0fd35146ed4c84f
austincripe <aaustinc@live.com>
2026-02-10 17:12:50 -0500

    update chatterbox image path

--- src/project_pannels/Chatapp.jsx
+++ src/project_pannels/Chatapp.jsx
@@ -28,13 +28,13 @@ function Chatapp({ setSelectedProject, selectedProject, windowSize }) {
             scale.set(1.2)
             opacity.set(1)
             // video.current.play()
-            gif.current.src = 'public/assets/images/projects/chatterbox/login-lobbychat.gif'
+            gif.current.src = '../assets/images/projects/chatterbox/login-lobbychat.gif'
         } else {
             scale.set(1)
             opacity.set(windowSize.width <= 950 ? 1 : 0)
             // video.current.pause()
             // video.current.currentTime = 0
-            gif.current.src = 'public/assets/images/projects/chatterbox/login.PNG'
+            gif.current.src = '../assets/images/projects/chatterbox/login.PNG'
         }
     }, [hovered])
     function Popout(){
